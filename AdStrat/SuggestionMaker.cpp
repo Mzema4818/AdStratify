c41ced5f0c7d228d9cbe834c2e6eee413adfc16f
Jenny <jennyzheng9999@gmail.com>
2024-12-11 22:59:24 -0500

    Wrote the code to suggest a better ad placement

--- AdStrat/SuggestionMaker.cpp
+++ AdStrat/SuggestionMaker.cpp
@@ -1,1 +1,25 @@
-// Made a file to suggest a better placement for the ad
+#include <string>
+#include <vector>
+#include "global.h"
+#include "RandomForest.h"
+
+using namespace std;
+
+// Function to suggest a better ad placement
+string suggestAdPlacement(const DataPoint& userPoint, const vector<string>& possiblePlacements, RandomForest& rf) {
+    for (const auto& placement : possiblePlacements) {
+        // Create a copy of the user's data point with a modified ad placement
+        DataPoint modifiedPoint = userPoint;
+        modifiedPoint.adPosition = placement;
+
+        // Predict the click outcome for the modified data point
+        int prediction = rf.predict(modifiedPoint);
+
+        // If the modified placement predicts a click (1), return the suggestion
+        if (prediction == 1) {
+            return placement;
+        }
+    }
+    // If no better placement is found, return the original placement
+    return userPoint.adPosition;
+}
---
+++ SuggestionMaker.cpp
@@ -0,0 +1,7 @@
+//
+//  SuggestionMaker.cpp
+//  
+//
+//  Created by Jenny Zheng on 12/11/24.
+//
+
