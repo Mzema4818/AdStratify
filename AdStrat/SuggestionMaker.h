3c445ea2274f77b245b8b13f42945fdd17991260
Jenny <jennyzheng9999@gmail.com>
2024-12-11 23:00:38 -0500

    Added a header file for SuggestionMaker

--- AdStrat/SuggestionMaker.h
+++ AdStrat/SuggestionMaker.h
@@ -1,1 +1,15 @@
+#ifndef SUGGESTIONMAKER_H
+#define SUGGESTIONMAKER_H
+
+#include <string>
+#include <vector>
+#include "global.h"
+#include "RandomForest.h"
+
+using namespace std;
+
+// Function to suggest a better ad placement
+string suggestAdPlacement(const DataPoint& userPoint, const vector<string>& possiblePlacements, RandomForest& rf);
+
+#endif // SUGGESTIONMAKER_H
 
